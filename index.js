8b7444577026afb695bc81619e8e682633380077
Azzan Khan <53059107+AzzanKhan@users.noreply.github.com>
2022-08-08 01:41:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const webSocket = require('ws')
+
+const server = new webSocket.Server({port:'8080'})
+
+server.on('connection', socket => {
+    
+    console.log('Client connected')
+    
+    socket.on('message', message => {
+        socket.send(`We received your ${message}`)
+    })
+    
+    socket.on('close', (event) => {
+        console.log('Client disconnected')
+    })
+
+})
+
+console.log('socket initialized on port 8080')
